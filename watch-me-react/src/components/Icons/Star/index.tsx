7321a630300117ccc70274c4bc1f80901e01196d
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-06 23:04:37 -0300

    feat: add star component

---
+++ watch-me-react/src/components/Icons/Star/index.tsx
@@ -0,0 +1,20 @@
+export function Star() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="17"
+      fill="none"
+      viewBox="0 0 17 17"
+    >
+      <path
+        fill="#F2C046"
+        stroke="#F2C046"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M8.5 1.417L10.69 5.85l4.894.715-3.541 3.45.836 4.873L8.5 12.587 4.123 14.89l.835-4.873-3.541-3.45 4.894-.715 2.19-4.434z"
+      ></path>
+    </svg>
+  );
+}
