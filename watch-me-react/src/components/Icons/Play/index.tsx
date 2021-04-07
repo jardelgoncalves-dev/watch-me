87f1fb9aee727c9213ee624b2448c9c2c5619bba
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-06 23:04:29 -0300

    feat: add play component

---
+++ watch-me-react/src/components/Icons/Play/index.tsx
@@ -0,0 +1,20 @@
+export function Play() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="48"
+      fill="none"
+      viewBox="0 0 48 48"
+    >
+      <path
+        fill="#F2C046"
+        stroke="#F2C046"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 6l28 18-28 18V6z"
+      ></path>
+    </svg>
+  );
+}
