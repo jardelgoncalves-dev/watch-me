3d717dad0ba98d1aff16ca44bae8ff993c01eda3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-08 23:34:45 -0300

    chore: select button component display

--- watch-me-react/src/App.tsx
+++ watch-me-react/src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from './components/Header'
+import { SelectButton, SelectButtonOptionProp } from './components/SelectButton'
 import './styles/global.scss';
 
 export function App() {
+  const options: SelectButtonOptionProp[] = [
+    { label: 'Acao', value: 'acao'},
+    { label: 'Drama', value: 'drama'},
+  ]
   return (
     <div className="app">
       <Header />
+      <SelectButton options={options} />
     </div>
   )
 }
