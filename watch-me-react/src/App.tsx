d9aa64b459557802254de508afdd661413b5e499
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-10 02:35:46 -0300

    feat: add home page component

--- watch-me-react/src/App.tsx
+++ watch-me-react/src/App.tsx
@@ -1,22 +1,13 @@
-import { Container } from './components/Container';
 import { Header } from './components/Header'
-import { SelectButton, SelectButtonOptionProp } from './components/SelectButton'
+import { HomePage } from './pages/Home';
 import './styles/global.scss';
 
 export function App() {
-  const options: SelectButtonOptionProp[] = [
-    { label: 'Acao', value: 'acao'},
-    { label: 'Drama', value: 'drama'},
-  ]
+  
   return (
     <div className="app">
       <Header />
-      <Container>
-        <div className="genre-select">
-          <h3>Genre</h3>
-          <SelectButton options={options} />
-        </div>
-      </Container>
+      <HomePage />
     </div>
   )
 }
