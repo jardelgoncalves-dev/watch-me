0774204358a2b9aa857e9142995a1a3ea8d98007
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-10 15:12:52 -0300

    feat: using list card movie and loading select button

--- watch-me-react/src/pages/Home/index.tsx
+++ watch-me-react/src/pages/Home/index.tsx
@@ -3,7 +3,9 @@ import { Col } from "../../components/Col";
 import { Container } from "../../components/Container";
 import { Row } from "../../components/Row";
 import { SelectButton, SelectButtonOptionProp } from "../../components/SelectButton";
+import { ListCardMovie } from "./components/ListCardMovie";
 import { LoadingCardMovie } from "./components/LoadingCardMovie";
+import { LoadingSelectButton } from "./components/LoadingSelectButton";
 
 import './styles.scss';
 
@@ -15,24 +17,23 @@ export function HomePage() {
     { label: 'Drama 3', value: 'drama 3'},
   ]
 
-  const movie = {
+  const movies = [{
     Title: "Underdog",
     Runtime: "84 min",
     Poster: "https://m.media-amazon.com/images/M/MV5BMTk5NjkyNzEwOV5BMl5BanBnXkFtZTcwODc5NDI1MQ@@._V1_SX300.jpg",
     imdbRating: "4.7",
-  }
+  }]
   
   return (
     <Container>
       <div className="genre-select">
         <h3>Genre</h3>
-        <SelectButton options={options} />
+        {/* <SelectButton options={options} /> */}
+        <LoadingSelectButton loading />
       </div>
       <Row>
         <LoadingCardMovie enabled quantity={5} />
-        <Col>
-          <CardMovie movie={movie} />
-        </Col>
+        <ListCardMovie movies={movies} />
       </Row>
     </Container>
   )
