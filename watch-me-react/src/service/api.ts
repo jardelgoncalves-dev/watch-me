28eecb70e6e7686d6901df218dd8079c34555363
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-10 18:09:37 -0300

    feat: add api service

---
+++ watch-me-react/src/service/api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const BASE_URL = 'http://localhost:3000';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+})
+
+api.interceptors.request.use(
+  config =>  config,
+  err => Promise.reject(err)
+)
+
+export { api }
